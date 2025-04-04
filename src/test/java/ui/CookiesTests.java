56a753de37f4d5d04f6788de3d21dad199fce51d
ISmeneger <ISmeneger@yandex.ru>
2025-04-04 18:08:49 +0300

    add ui Cookie tests

--- src/test/java/ui/CookiesTests.java
+++ src/test/java/ui/CookiesTests.java
@@ -1,6 +1,5 @@
 package ui;
 
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.assertj.core.api.AssertionsForInterfaceTypes;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,19 +14,12 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static steps.WebFormSteps.openCookiesPage;
 
 public class CookiesTests {
     WebDriver driver;
     private static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
-
-    @BeforeEach
-    void start() throws InterruptedException {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(BASE_URL);
-        openCookiesPage(driver);
-    }
 
     @AfterEach
     void tearDown() {
@@ -36,26 +28,38 @@ public class CookiesTests {
 
     @Test
     void cookieTest() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
+        openCookiesPage(driver);
+
+        String textBefore = "username=John Doe\ndate=10/07/2018";
+        String textBlock = "username=John Doe\ndate=10/07/2018\nNew President=Donald Trump";
+        String textAfter = "username=John Doe\nNew President=Donald Trump";
+
         WebDriver.Options options = driver.manage();
         WebElement displayCookies = driver.findElement(By.id("refresh-cookies"));
-        WebElement cookies_list = driver.findElement(By.id("cookies-list"));
+        WebElement cookiesList = driver.findElement(By.id("cookies-list"));
         displayCookies.click();
 
-        assertEquals("username=John Doe\n" + "date=10/07/2018", cookies_list.getText());
+        assertEquals(textBefore, cookiesList.getText());
 
         Set<Cookie> cookies = options.getCookies();
         assertThat(cookies).hasSize(2);
 
         Cookie date = options.getCookieNamed("date");
+        assert date != null;
+        assertNotNull(date.getValue(), "date.getValue() should be not null");
         assertEquals("10/07/2018", date.getValue());
-        assertThat(date.getPath()).isEqualTo("/");
+        assertThat(date.getPath()).isEqualTo("/")
+                .as(String.format("date.getPath() should be %s, but was %s", "/", date.getPath()));
         Thread.sleep(2000);
 
         Cookie addNewCookie = new Cookie("New President", "Donald Trump");
         options.addCookie(addNewCookie);
         displayCookies.click();
 
-        assertEquals("username=John Doe\n" + "date=10/07/2018\n" + "New President=Donald Trump", cookies_list.getText());
+        assertEquals(textBlock, cookiesList.getText());
 
         String readValue = options.getCookieNamed(addNewCookie.getName()).getValue();
         System.out.println(readValue);
@@ -70,7 +74,8 @@ public class CookiesTests {
         Thread.sleep(2000);
 
         displayCookies.click();
-        assertEquals("username=John Doe\n" + "New President=Donald Trump", cookies_list.getText());
+
+        assertEquals(textAfter, cookiesList.getText());
         Thread.sleep(2000);
     }
 }
